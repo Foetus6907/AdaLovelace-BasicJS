d6c088ed9be8eaf898d2dc3f1f444ab75b3a65ff
mghoubali <moha.g@hotmail.fr>
2020-09-01 15:17:14 +0200

    testjest

--- JestTest/user.test.js
+++ JestTest/user.test.js
@@ -1,9 +1,35 @@
-const assert = require('assert')
+require('jest')
 
 class User {
+  constructor(name, lastname, age) {
+    this.name = name
+    this.age = age
+    this.fullName = `${this.name} ${lastname}`
+  }
 
+  ageUp(){
+    this.age += 1
+  }
 }
 
-const user = new User('Thomas')
-assert.equal(user.nom, 'Falcone')
-assert.equal(user.prenom, 'Thomas')
+const user = new User('Thomas', 'Pasquier', 24)
+
+describe('User  focntion', () => {
+  it('return name', () => {
+    expect(user.name).toBe('Thomas')
+  })
+  it('return age', () => {
+    expect(user.age).toBe(24)
+  })
+
+  it('should incremented age by one', () => {
+    expect(user.age).toBe(24)
+    user.ageUp()
+    expect(user.age).toBe(25)
+  });
+
+  it('should return full name', () => {
+    expect(user.fullName).toBe('Thomas Pasquier')
+  });
+
+})
